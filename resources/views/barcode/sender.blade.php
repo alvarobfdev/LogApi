0b720224e82df5414a12665bdfa40f4a5fc14ccd
Álvaro Baño Fos <alvarobanofos@gmail.com>
2016-06-28 11:58:11 +0200

    barcode server

--- resources/views/barcode/sender.blade.php
+++ resources/views/barcode/sender.blade.php
@@ -1,6 +1,6 @@
 @extends('base')
 @section('content')
-    <form method="post" id="form">
+    <form method="post" id="form" name="form">
         <input id="code" type="text" name="code" onchange="sendForm()">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
     </form>
@@ -9,7 +9,7 @@
     <script>
         document.getElementById("code").addEventListener('change', sendForm, false);
         setTimeout(function() { document.getElementById('code').focus(); }, 0);
-
+        document.form.elements[0].focus();
         function sendForm() {
             document.getElementById("form").submit();
         }
